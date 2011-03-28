456cb38fd2d58c4bfe974f24368edd77ad9e2759
Mitchell Cooper <cooper879@msn.com>
2011-03-28 00:44:26 +0000

    0.9.0: Completely rewrote handle.pm due to bugs and other issues.

--- handle.pm
+++ handle.pm
@@ -1,47 +1,90 @@
 #!/usr/bin/perl -w
+# Copyright (c) 2011, Mitchell Cooper
 package handle;
+
 use warnings;
 use strict;
-use less 'mem';
+use feature 'switch';
+
 use utils qw/col validnick conf/;
-sub new {
+
+sub user {
     my $user = user::lookup(shift) or return;
-    my $all = shift;
-    foreach my $data (split "\n", $all) {
-        $data =~ s/\s+$//;
-        $data =~ s/^\s+//;
-        return if $data eq '';
-        ($user->{'ping'},$user->{'last'}) = (time,time);
-        my @s = split / /, $data;
-        $user->{'idle'} = time unless $s[0] =~ m/^(PONG|PING)$/i;
+    my $data = shift;
+    foreach my $line (split "\n", $data) {
+
+        # strip characters that might interfere later
+        $line =~ s/(\0|\r|\n)//g;
+        my @s = split / /, $line;
+
+        # ignore empty lines
+        next unless length $line;
+
+        # reset idle times, ping times, etc.
+        user_reset_timer($user, $s[0]);
+
         if ($user->{'ready'}) {
-            $user->handle($s[0],$data);
-        } else {
-            if (uc($s[0]) eq 'NICK') {
-                if ($s[1]) {
-                    if (validnick($s[1],conf('limit','nick'),undef)) {
-                        unless(user::nickexists($s[1])) {
-                            $user->{'nick'} = $s[1];
-                            if (exists $user->{'ident'}) {
-                                $user->{'ready'} = 1;
-                                $user->start;
-                            }
-                        } else { $user->sendserv('433 '.$user->nick.' '.$s[1].' :Nickname is already in use.'); }
-                    } else { $user->sendserv('432 '.$user->nick.' '.$s[1].' :Erroneous nickname'); }
-                } else { $user->sendnum(431,':No nickname given'); }
-            } elsif (uc($s[0]) eq 'USER') { 
-                if (exists $s[4]) {
-                    if (validnick($s[1],conf('limit','ident'),1)) {
-                        $user->{'gecos'} = col((split / /, $data, 5)[4]);
-                        $user->{'ident'} = '~'.$s[1];
-                        if (exists $user->{'nick'}) {
-                            $user->{'ready'} = 1;
-                            $user->start;
-                        }
-                    } else { $user->sendserv('461 '.$user->nick.' USER :Your username is not valid'); }
-                } else { $user->sendserv('461 '.$user->nick.' USER :Not enough parameters'); }
-            } else { $user->sendserv('421 '.$user->nick.' '.uc($s[0]).' :Unknown command') unless uc($s[0]) eq 'PONG' or uc($s[0]) eq 'CAP'; }
+            $user->handle($s[0], $line);
+            next
+        }
+        given (lc $s[0]) {
+            when ('nick') {
+                user_handle_nick($user, @s)
+            } when('user') {
+                user_handle_user($user, $line, @s)
+            } default {
+                $user->numeric(421, uc $s[0]) if lc $s[0] !~ m/^(PONG|PING|CAP)$/
+            }
         }
     }
+    return 1
 }
+
+sub user_handle_user {
+    my ($user, $data, @s) = @_;
+    if (!defined $s[4]) {
+        $user->numeric(461, 'USER');
+        return
+    }
+    if (validnick($s[1], conf('limit', 'ident'), 1)) {
+        $user->{'gecos'} = col((split / /, $data, 5)[4]);
+        $user->{'ident'} = '~'.$s[1];
+        user_start($user) if exists $user->{'nick'};
+        return 1
+    }
+}
+
+sub user_handle_nick {
+    my ($user, @s) = @_;
+    if (!defined $s[1]) {
+        $user->numeric(461, 'NICK');
+        return
+    }
+    if (validnick($s[1], conf('limit', 'nick'), undef)) {
+        if (!user::nickexists($s[1])) {
+            $user->{'nick'} = $s[1];
+            user_start($user) if exists $user->{'ident'};
+            return 1
+        } else {
+            $user->numeric(433, $s[1])
+        }
+    } else {
+        $user->numeric(432, $s[1])
+    }
+}
+
+sub user_reset_timer {
+    my ($user, $command) = @_;
+    $user->{'idle'} = time if $command !~ m/^(ping|pong)$/i;
+    $user->{'last'} = time;
+    $user->{'ping'} = time;
+    return 1
+}
+
+sub user_start {
+    my $user = shift;
+    $user->{'ready'} = 1;
+    $user->start
+}
+
 1

2eed04ea6f8fa3b2a66373b215dd3c1499518546
Mitchell Cooper <cooper879@msn.com>
2011-04-09 19:58:24 +0000

    cleanup

--- user.pm
+++ user.pm
@@ -17,24 +17,31 @@ our %commands;
 # register the command handlers in userhandlers.pm
 &userhandlers::get;
 
+# main command handler
 sub handle {
-    # main command handler
     my ($user, $command) = (shift, uc shift);
+
     if (exists $commands{$command}) {
-        # call to the CODE ref
+
+        # call the CODE
         $commands{$command}{'code'}($user, shift)
+
     }
+
+    # unknown command
     else {
-        # unknown command
         $user->numeric(421, $command)
     }
+
 }
 
+# create a new user
 sub new {
     my ($ssl, $peer) = @_;
     return unless $peer;
+
+    # the server is not accepting connections
     if (!&acceptcheck) {
-        # the server is not accepting connections
         $peer->close;
         return
     }
@@ -42,8 +49,10 @@ sub new {
     # check and make sure the IP is not Z-Lined, blacklisted, or if it has reached its max-per-IP limit
     my @ip_accept = ip_accept($peer->peerhost);
     if (!$ip_accept[0]) {
+
         # we can't use main::sendpeer here because the outbuffer will be cleared before the main loop gets a chance to send the data
         $peer->syswrite('ERROR :Closing Link: (*@'.$peer->peerhost.') ['.$ip_accept[1].']'."\r\n", POSIX::BUFSIZ);
+
         $peer->close;
         return
     }
@@ -76,19 +85,24 @@ sub new {
     return $user
 }
 
+# this is where the actual mode setting is done; mode handling is done in hmodes()
 sub setmode {
-    # this is where the actual mode setting is done; mode handling is done in hmodes()
     my ($user, $modes, $silent) = @_;
     $user->send(':'.$user->nick.' MODE '.$user->nick.' :+'.$modes) unless $silent;
     foreach (split //, $modes) {
         $user->{'mode'}->{$_} = time;
+
+        # don't do anything for these modes
         next if $_ =~ m/i/;
+
         if ($_ eq 'x' && conf qw/enabled cloaking/) {
+
             # use . for IPv4 and : for IPv6
             my $sep = ($user->{'ipv'} == 6 ? ':' : '\.');
 
             # set the hidden host
-            $user->setcloak(host2cloak($sep, $user->{'host'}));
+            $user->setcloak(host2cloak($sep, $user->{'host'}))
+
         }
     }
 }
@@ -99,22 +113,27 @@ sub ismode {
     return
 }
 
+# this is where the actual mode unsetting is done; mode handling is done in hmodes()
 sub unsetmode {
-    # once again, this is where the actual mode unsetting is done; mode handling is done in hmodes()
     my ($user, $modes, $silent) = @_;
     $user->send(':'.$user->nick.' MODE '.$user->nick.' :-'.$modes) unless $silent;
     foreach (split //, $modes) {
         delete $user->{'mode'}->{$_};
         next if $_ =~ m/(i|S)/;
         if ($_ eq 'x' && conf qw/enabled cloaking/) {
+
             # restore the original cloak
             $user->unsetcloak;
-        } elsif ($_ eq 'o') {
+
+        }
+        elsif ($_ eq 'o') {
+
             # delete the user's IRCop
             delete $user->{'oper'};
 
             # unset server notices if set
             $user->unsetmode('S') if $user->ismode('S');
+
         }
     }
 }
@@ -132,13 +151,18 @@ sub hmodes {
         given ($piece) {
             when ('+') {
                 $state = 1
-            } when ('-') {
+            }
+            when ('-') {
                 $state = 0
-            } when ('Z') {
-                # modes that cannot be set or unset
+            }
+
+            # modes that cannot be set or unset
+            when ('Z') {
                 next
-            } when (/(o|S)/) {
-                # oper-only modes
+            }
+
+            # oper-only modes
+            when (/(o|S)/) {
                 if ($user->ismode('o')) {
                     if ($state) {
                         $user->setmode($piece)
@@ -148,22 +172,28 @@ sub hmodes {
                     }
                 }
                 # otherwise just ignore it
-            } when ($_ ~~ @enabled_modes) {
-                # modes that actually exist!
+            }
+
+            # normal modes
+            when ($_ ~~ @enabled_modes) {
                 if ($state) {
                     $user->setmode($piece)
                 }
                 else {
                     $user->unsetmode($piece)
                 }
-            } default {
-                # unknown mode
+            }
+
+            # unknown mode
+            default {
                 $user->numeric(501, $piece)
             }
+
         }
     }
 }
 
+# set the displayed host of a user
 sub setcloak {
     my ($user, $cloak) = @_;
     $user->numeric(396, $cloak);
@@ -171,6 +201,7 @@ sub setcloak {
     return $cloak
 }
 
+# create an SHA cloak of a host
 sub host2cloak {
     my @pieces = ();
     my $sep = shift;
@@ -189,14 +220,15 @@ sub host2cloak {
     return (join $sep, @pieces)
 }
 
+# restore original host
 sub unsetcloak {
-    # restore a user's original host
     my $user = shift;
     $user->numeric(396, $user->host);
     $user->{'cloak'} = $user->host;
     return $user->host
 }
 
+# find a user by their socket object
 sub lookup {
     my $peer = shift;
     return $connection{$peer} if exists $connection{$peer};
@@ -205,12 +237,13 @@ sub lookup {
     return
 }
 
+# send data
 sub send {
-    ::sendpeer(shift->obj, @_)
+    return ::sendpeer(shift->obj, @_)
 }
 
+# check for an oper flag
 sub can {
-    # check for an oper flag
     my ($user, $priv) = @_;
     return unless defined $user->{'oper'};
     foreach (split / /, oper($user->{'oper'}, 'privs')) {
@@ -219,7 +252,10 @@ sub can {
 
     # they don't have that priv
     return
+
 }
+
+# user quit
 sub quit {
     my ($user, $reason, $silent, $display) = @_;
 
@@ -257,95 +293,101 @@ sub quit {
 
     # double-check if the server is ready to accept new connections
     &acceptcheck
+
+    return 1
 }
+
+# send a NOTICE from the server
+# :server NOTICE nick :message
 sub servernotice {
-    # send a NOTICE from the server
-    # :server NOTICE nick :message
     my $user = shift;
-    $user->send(':'.(conf qw/server name/).' NOTICE '.$user->nick." :@_");
+    return $user->send(':'.(conf qw/server name/).' NOTICE '.$user->nick." :@_")
 }
 
+# server notice for a command such as CHGHOST
 sub snt {
-    # server notice for a command such as CHGHOST
-    shift->servernotice(sprintf '*** %s: %s', shift, shift)
+    return shift->servernotice(sprintf '*** %s: %s', shift, shift)
 }
 
+# send a numeric
+# deprecated; use numeric() instead.
+# (this is still used in the start() function as those numerics are only used once,
+# or at least until the VERSION command is complete.)
 sub sendnum {
-    # deprecated; use numeric() instead.
-    # (this is still used in the start() function as those numerics are only used once,
-    # or at least until the VERSION command is complete.)
     my $user = shift;
-    $user->send(':'.(conf qw/server name/).' '.shift().' '.$user->nick." @_")
+    return $user->send(':'.(conf qw/server name/).' '.shift().' '.$user->nick." @_")
 }
 
+# new way to send a numeric
+# numerics are defined in the %utils::numerics hash
+# a single numeric can have multiple strings since int() is used.
 sub numeric {
-    # send a numeric
-    # numerics are defined in the %numerics hash
     my ($user, $num) = (shift, shift);
-    $user->send(join q. ., ':'.(conf qw/server name/), (int $num), $user->nick, (sprintf $utils::numerics{$num}, @_))
+    return $user->send(join q. ., ':'.(conf qw/server name/), (int $num), $user->nick, (sprintf $utils::numerics{$num}, @_))
 }
 
+# send data from the server
 sub sendserv {
-    # send from the server
-    # :server data
-    shift->send(':'.(conf qw/server name/).' '.(sprintf shift, @_))
+    return shift->send(':'.(conf qw/server name/).' '.(sprintf shift, @_))
 }
 
+# send from the server, join()ing the arguments by a space
 sub sendservj {
-    # send from the server, using join(' ') for each argument
-    shift->send(':'.(conf qw/server name/).' '.(join q. ., @_))
+    return shift->send(':'.(conf qw/server name/).' '.(join q. ., @_))
 }
 
+# send data from a server or user
 sub sendfrom {
-    # send from a server or user
-    shift->send(':'.shift().' '.(sprintf shift, @_))
+    return shift->send(':'.shift().' '.(sprintf shift, @_))
 }
 
+# send from a server or user, using join(' ') for each argument
 sub sendfromj {
-    # send from a server or user, using join(' ') for each argument
-    shift->send(':'.shift().' '.(join q. ., @_))
+    return shift->send(':'.shift().' '.(join q. ., @_))
 }
 
+# the entire mask, using the displayed host
 sub fullcloak {
-    # the entire mask, using the displayed host
     my $user = shift;
     return $user->{'nick'}.'!'.$user->{'ident'}.'@'.$user->{'cloak'} if $user->{'ready'};
     return '*'
 }
 
+# sends a privmsg to a user
+# who knows why this has its own function? it's only used once...
 sub recvprivmsg {
-    # who knows why this has its own function? it's only used once...
-    my ($user,$from,$target,$msg,$cmd) = @_;
-    $user->send(':'.(join q. ., $from, $cmd, $target).' :'.$msg)
+    my ($user, $from, $target, $msg, $cmd) = @_;
+    return $user->send(':'.(join q. ., $from, $cmd, $target).' :'.$msg)
 }
 
+# look for a usermode set on the user
 sub mode {
-    # look for a usermode set on the user
     my ($user, $mode) = @_;
 
-    # found one
+    # found it
     return $user->{'mode'}->{$mode} if exists $user->{'mode'}->{$mode};
 
-    # doesn't exit
+    # doesn't have mode set
     return
+
 }
 
+# the entire mask, using the actual host
 sub fullhost {
-    # the entire mask, using the actual host
     my $user = shift;
     return $user->{'nick'}.'!'.$user->{'ident'}.'@'.$user->{'host'} if defined $user->{'ready'};
     return '*'
 }
 
+# user's nick
 sub nick {
-    # user's nick or * if they haven't registered
     my $user = shift;
     return $user->{'nick'} if $user->{'nick'};
     return '*'
 }
 
+# called by handle.pm after the user registers
 sub start {
-    # called by handle.pm after the user registers
     my $user = shift;
     return if $user->checkkline;
     snotice('client connecting: '.$user->fullhost.' ['.$user->{'ip'}.']');
@@ -365,46 +407,49 @@ sub start {
     return 1
 }
 
+# a new user ID (ID, not UID)
 sub newid {
-    # a new ID
     $utils::GV{'cid'}++;
     return $utils::GV{'cid'}-1
 }
 
+# the user's UID
 sub id {
-    # the user's UID
     return shift->{'id'}
 }
 
+# the user's IO::Socket object
 sub obj {
-    # the user's IO::Socket object
     return shift->{'obj'}
 }
 
+# the ID of the server the user is on
 sub server {
-    # the ID of the server the user is on
     return shift->{'server'}
 }
 
+# user's acutal host
 sub host {
-    # user's acutal host
     return shift->{'host'}
 }
 
+# check if a nickname exists and return that user's object if it does
 sub nickexists {
-    # check if a nickname exists and return that user's object if it does
     my $nick = shift;
     foreach (values %connection) {
+
         # found a match
         return $_ if lc $_->{'nick'} eq lc $nick
+
     }
 
     # no such nick
-    return    
+    return
+
 }
 
+# find a user by their UID
 sub lookupbyid {
-    # find a user by their UID
     my $id = shift;
     foreach (values %connection) {
         # found a match
@@ -412,12 +457,13 @@ sub lookupbyid {
     }
 
     # no such UID
-    return 
+    return
+
 }
 
+# check if a user has correct oper credentials
+# TODO: add support for SHA encryption.
 sub canoper {
-    # check if a user has correct oper credentials
-    # TODO: add support for SHA encryption.
     my ($user, $oper, $password) = @_;
     return unless exists $::oper{$oper};
 
@@ -426,24 +472,27 @@ sub canoper {
 
                 # check if the mask is correct
                 return $oper if hostmatch($user->fullhost, (split / /, oper($oper, 'host')))
+
     }
 
     # invalid credentials
     return
+
 }
 
+# check if a user is on a channel (by objects, not names)
 sub ison {
-    # check if a user is on a channel (by objects, not names)
     my ($user, $channel) = @_;
     return 1 if exists $channel->{'users'}->{$user->{'id'}};
     return
 }
 
+# check if the user's mask matches a K-Line in the configuration
+# if it does, force them to quit
 sub checkkline {
-    # check if the user's mask matches a K-Line in the configuration
     my $user = shift;
     foreach (keys %::kline) {
-        if (hostmatch($user->fullhost,$_)) {
+        if (hostmatch($user->fullhost, $_)) {
             # found a match; forcing them to quit
             $user->quit('K-Lined: '.$::kline{$_}{'reason'}, undef, 'K-Lined'.((conf qw/main showkline/) ? ': '.$::kline{$_}{'reason'}:''));
             return 1
@@ -452,28 +501,32 @@ sub checkkline {
 
     # they're free to go
     return
+
 }
 
+# ensure that the server is accepting connections
 sub acceptcheck {
-    # ensure that the server is accepting connections
-    my $i = 0;
-    $i++ foreach (values %connection);
+    my $i = scalar keys %connection;
+
+    # maximum client count reached
     if ($i == conf qw/limit clients/) {
-        # maximum client count reached
         snotice('No new clients are being accepted. ('.$i.' users)') if $::ACCEPTING != 0;
         $::ACCEPTING = 0;
         return
+
     }
+
+    # person(s) quit; accepting clients again
     else {
-        # person(s) quit; accepting clients again
         snotice('Clients are now being accepted. ('.$i.' users)') if $::ACCEPTING != 1;
         $::ACCEPTING = 1;
         return 1
     }
+
 }
 
+# make sure the max-per-ip limit has not been reached and that the IP is not zlined.
 sub ip_accept {
-    # make sure the max-per-ip limit has not been reached and that the IP is not zlined.
     my $ip = shift;
     my $count = 0;
     foreach (values %connection) {
@@ -487,21 +540,29 @@ sub ip_accept {
             # IP matches a Z-Line in the configuration
             return (undef, 'Z-Lined: '.$::zline{$_}{'reason'}) if hostmatch($ip, $_)
     }
+
+    # they're free to go
     return 1
+
 }
 
+# import the selected encrypting
 sub DigestImport {
     say '        Importing SHA1 support to cloaking module';
     Digest::SHA->import('sha1_hex')
 }
 
+# add a command handler
+# do NOT call this from an API module.
+# see API::Command to do that.
 sub register_handler {
-    # add a command handler
     my ($handler, $code, $source, $desc) = (uc shift, shift, shift, shift);
     if (exists $commands{$handler}) {
+
         # command already exists
         say 'register_handler failed; '.$handler.' already exists.';
         return
+
     }
 
     # success
@@ -512,10 +573,12 @@ sub register_handler {
     };
     say $source.' registered handler '.$handler.': '.$desc;
     return 1
+
 }
 
+# delete a command handler
 sub delete_handler {
-    # delete a command handler
+    
     my $command = uc shift;
 
     # if it exists, delete it
@@ -527,6 +590,7 @@ sub delete_handler {
     # it doesn't
     say 'delete_handler failed; '.$command.' does not exist.';
     return
+
 }
 
 1
